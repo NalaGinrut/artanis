23204c10c3f5f2f71ad4f2838edac7a816b7de78
Nala Ginrut <nalaginrut@gmail.com>
2013-02-19 16:08:47 +0800

    fixed examples/test.scm

--- examples/test.scm
+++ examples/test.scm
@@ -1,15 +1,17 @@
 #! /usr/local/bin/guile \
--L ./
+-L ../
 !#
 
 
 (use-modules (artanis artanis))
+(init-server)
+
 (define mmr "123")
 
 (get "/mmr/:id1/to/:id2"
   (lambda (rc)
-    (let ((id1 (params rc 'id1))
-          (id2 (params rc 'id2)))
+    (let ((id1 (params rc "id1"))
+          (id2 (params rc "id2")))
       (response-emit (format #f "~a: Send from ~a to ~a~%" mmr id1 id2)))))
 
 (run)
