48e67c266c64395eca10c1efff3c52e20facad9a
Nala Ginrut <nalaginrut@gmail.com>
2014-03-12 21:46:03 +0800

    Rewrite DB module. Dropped object-oriented, using record-type.

--- artanis/db.scm
+++ artanis/db.scm
@@ -1,5 +1,5 @@
 ;;  -*-  indent-tabs-mode:nil; coding: utf-8 -*-
-;;  Copyright (C) 2013
+;;  Copyright (C) 2013,2014
 ;;      "Mu Lei" known as "NalaGinrut" <NalaGinrut@gmail.com>
 ;;  Artanis is free software: you can redistribute it and/or modify
 ;;  it under the terms of the GNU General Public License as published by
@@ -16,122 +16,135 @@
 
 (define-module (artanis db)
   #:use-module (artanis utils)
+  #:use-module (artanis config)
   #:use-module (dbi dbi)
-  #:use-module (oop goops))
+  #:use-module (ice-9 match)
+  #:use-module ((rnrs) #:select (define-record-type)))
 
-(module-export-all! (current-module))
+;; TODO: add init-db in init-server for initializing connection pool
 
-;; monitor of DB, you have to register the DB you choosed before you use 
-;; <artanis-db> serial classes. Or it can't be monitored.
-(define current-artanis-db (make-parameter #f)) 
+;; NOTE:
+;; <db> is only used for store connect config info, it doens't contain
+;; connection object. It's useless when the connection-pool init is down.
+(define-record-type <db>
+  (fields 
+   username passwd))
 
-(define-class <artanis-db> ()
-  (db #:init-value #f #:getter db-get #:setter db-set!))
+(define-record-type <mysql>
+  (parent <db>)
+  ;; mysql provides two modes, addr:port or socketfile
+  ;; the default values:
+  ;; port: 3306, addr: localhost, dbname: artanis
+  (fields dbname port addr socketfile))
 
-(define-generic conn)
-(define-generic re-conn)
-(define-generic close)
-(define-generic insert)
-(define-generic query)
-(define-generic get-status)
+(define-syntax-rule (->mysql mysql)
+  (match mysql
+    (($ <mysql> ($ <db> _ username passwd) dbname port addr #f)
+     (format #f "~a:~a:~a:tcp:~a:~a" username passwd dbname addr port))
+    (($ <mysql> ($ <db> _ username passwd) dbname #f #f socketfile)
+     (format #f "~a:~a:~a:socket:~a" username passwd dbname socktfile))
+    (else (error 'mysql "Wrong connection config!" mysql))))
 
-(define-method (insert (self <artanis-db>) (table <string>) . vals)
-  (let ((vstr (string-join vals ",")))
-    (query self (format #f "insert into ~a values (~a)" table vstr))))
+(define-record-type <sqlite3>
+  (parent <db>)
+  ;; sqlite3 only need dbname to open
+  ;; default dbname: artanis
+  (fields dbname))
 
-(define-method (close (self <artanis-db>))
-  (dbi-close (db-get self))
-  (db-set! self #f))
+(define-syntax-rule (->sqlite3 sqlite3)
+  (match sqlite3
+    (($ <sqlite3> ($ <db> _ username passwd) dbname)
+     dbname) ; FIXME: is it necessary for sqlite3 to require username/passwd??
+    (else (error 'sqlite3 "Wrong connection config!" sqlite3))))
 
-(define-method (query (self <artanis-db>) (sql <string>))
-  (dbi-query (db-get self) sql))
+(define-record-type <postgresql>
+  (parent <db>)
+  ;; postgresql need addr:port to open
+  (fields dbname port addr))
 
-(define *status-list*
-  '(ok))
-;; TODO: finish other status values
-(define-syntax-rule (status->symbol status)
-  (list-ref *status-list* (car status)))
-(define-method (get-status (self <artanis-db>))
-  (dbi-get_status (db-get self)))
+(define-syntax-rule (->postgresql postgresql)
+  (match postgresql
+    (($ <postgresql> ($ <db> _ username passwd) dbname port addr)
+     (format #f "~a:~a:~a:tcp:~a:~a" username passwd dbname addr port))
+    (else (error 'postgresql "Wrong connection config!" postgresql))))
 
-(define-method (check-status (self <artanis-db>))
-  (let ((st (get-status self)))
-    (values (car st) (cdr st))))
+;; NOTE:
+;; DB-conn! returns dbi connection object
+;; Use dbi-query on it!
+(define (DB-conn! db)
+  (cond
+   ((<mysql>? db) (dbi-open "mysql" (->mysql db)))
+   ((<sqlite3>? db) (dbi-open "sqlite3" (->sqlite3 db)))
+   ((<postgresql>? db) (dbi-open "postgresql" (->postgresql db)))
+   (else (error DB-conn! "Invalid DB!" db))))
 
-(define-method (get-all-rows (self <artanis-db>))
-  (let ((db (db-get self)))
-    (let lp((next (dbi-get_row db)) (result '()))
-      (if next
-          (lp (dbi-get_row db) (cons next result))
-          (reverse result)))))
+;; ---------------------conn operations-------------------------------
+;; NOTE: conn objects are actually dbi object!
+(define DB-query dbi-query)
 
-(define-method (get-one-row (self <artanis-db>))
-  (dbi-get_row (db-get self)))
+;; NOTE: actually it never close the connection, just recycle it.
+(define (DB-close conn)
+  ;; TODO: implemnt current-connection-pool
+  (conn-pool-in! (current-connection-pool) conn)) 
 
-;; NOTE: don't store passwd
-(define-class <mysql> (<artanis-db>)
-  (user #:init-value "artanis" #:init-keyword #:user 
-        #:getter db-user #:setter db-user!)
-  (name #:init-value "artanis" #:init-keyword #:name
-        #:getter db-name #:setter db-name!)
-  (port #:init-value "3306" #:init-keyword #:port
-        #:getter db-port #:setter db-port!)
-  (addr #:init-value "tcp:localhost" #:init-keyword #:addr
-        #:getter db-addr #:setter db-addr!))
- 
-(define-method (conn (self <mysql>) (passwd <string>))
-  (let ((user (db-user self))
-        (name (db-name self))
-        (port (db-port self))
-        (addr (db-addr self)))
-    (db-set! self (dbi-open "mysql"
-                            (format #f "~a:~a:~a:~a:~a"
-                                    user passwd name addr port)))
-    self))
+(define DB-status dbi-get_status)
 
-(define-method (re-conn (self <mysql>) (user <string>)
-                        (passwd <string>) (addr <string>))
-  (and user (db-user! self user))
-  (and addr (db-addr! self user))
-  (close self)
-  (conn self passwd))
+(define (DB-get-all-rows conn)
+  (let lp((next (dbi-get_row conn)) (result '()))
+    (if next
+        (lp (dbi-get_row conn) (cons next result))
+        (reverse result))))
+;;--------------------------------------------------------------------
 
-(define-class <sqlite3> (<artanis-db>)
-  (db-name #:init-value "artanis" #:init-keyword #:db-name #:getter db-name))
+(define (new-DB)
+  ;; TODO: 
+  ;; 1. Implement a new config module
+  ;; 2. Add a new global var to hold DB object (or global env table?).
+  ;;    Init new DB on the fly is not allowed.
+  (let ((db (get-conf 'database)))
+    ;; (get-conf 'database) should contain username passwd and connect method
+    (match db
+      (('mysql username passwd dbname ('socketfile socketfile)) 
+       (make-<mysql> username passwd #f #f socketfile))
+      (('mysql username passwd dbname ('port addr port))
+       (make-<mysql> username passwd port addr #f))
+      (('sqlite3 username passwd dbname)
+       (make-<sqlite3> username passwd dbname))
+      (('postgresql username passwd dbname ('port addr port))
+       (make-<postgresql> username passwd port addr)))))
 
-(define-method (conn (self <sqlite3>))
-  (let ((name (db-name self)))
-    (db-set! self (dbi-open "sqlite3" name))
-    self))
+(define-record-type <connection-pool>
+  (fields 
+   (mutable size)
+   ;; mutex ; maybe it's unecessary for green-thread
+   (mutable pool)))
 
-(define-method (re-conn (self <sqlite3>))
-  (close self)
-  (conn self))
+(define (conn-pool-out! cp)
+  (let ((size (<connection-pool>-size cp))
+        (pool (<connection-pool>-pool cp)))
+  (<connection-pool>-size-set! cp (1- size))
+  ;; lock ; maybe it's unecessary for green-thread
+  (queue-out! pool)))
 
-(define-class <postgresql> (<artanis-db>)
-  (user #:init-value "artanis" #:init-keyword #:user
-        #:getter db-user #:setter db-user!)
-  (name #:init-value "artanis" #:init-keyword #:name
-        #:getter db-name #:setter db-name!)
-  (addr #:init-value "tcp:localhost"
-        #:init-keyword #:addr #:getter db-addr #:setter db-addr!)
-  (port #:init-value "5432" #:init-keyword #:port 
-        #:getter db-port #:setter db-port!))
+(define (conn-pool-in! cp conn)
+  (let ((size (<connection-pool>-size cp))
+        (pool (<connection-pool>-pool cp)))
+    (<connection-pool>-size-set! cp (1+ size))
+    (queue-in! pool conn)))
 
-(define-method (conn (self <postgresql>) (passwd <string>))
-  (let ((user (db-user self))
-        (name (db-name self))
-        (port (db-port self))
-        (addr (db-addr self)))
-    (db-set! self (dbi-open "postgresql"
-                        (format #f "~a:~a:~a:~a:~a" 
-                                user passwd name addr port)))
-    self))
+(define (new-connection-pool)
+  (let* ((pool-size (get-conf 'pool-size))
+         (db (new-DB))
+         (conns (map (lambda (i) (DB-conn! db)) (iota pool-size)))
+         (pool (list->queue conns)))
+    (make-<connection-pool> pool-size pool)))
 
-(define-method (re-conn (self <postgresql>) (user <string>) (name <string>)
-                        (passwd <string>) (addr <string>))
-  (and user (db-user! self user))
-  (and name (db-name! self name))
-  (and addr (db-addr! self user))
-  (close self)
-  (conn self passwd))
+(define (init-connection-pool)
+  ;; TODO: create pool and add it to global
+  )
+
+(define (init-DB)
+  ;; TODO:
+  ;; 1. create a new db object
+  ;; 2. create a new connection pool
+  )
