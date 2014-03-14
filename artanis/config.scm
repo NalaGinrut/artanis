3d11b0135ebd1758e10c5815b8886a8e6c3c797d
Nala Ginrut <nalaginrut@gmail.com>
2014-03-14 18:54:50 +0800

    done config module

--- artanis/config.scm
+++ artanis/config.scm
@@ -15,24 +15,156 @@
 ;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (artanis config)
+  #:use-module (artanis version)
   #:use-module (ice-9 rdelim)
   #:use-module (ice-9 match)
-  #:export (init-config))
+  #:export (init-config get-conf))
 
-(define server-info "Artanis-0.0.1")
+(define server-info artanis-version)
+(define *default-conf-file* "/etc/artanis/default.conf")
+
+(define *conf-hash-table* (make-hash-table))
+(define (conf-set! k v)
+  (hash-set! *conf-hash-table* k v))
+
+(define-syntax-rule (->bool x)
+  (case (string->symbol (string-downcase x))
+    ((true on yes enable) #t)
+    ((fasle off no disable) #f)
+    (else (error "Invalid boolean item!" x))))
+
+(define-syntax-rule (->list x)
+  (map (lambda (e)
+         (string->symbol (string-trim-both e)))
+       (string-split x #\,)))
+
+(define-syntax-rule (->none/str x)
+  (case (string->symbol (string-downcase x))
+    ((none null #{}#) "")
+    (else x)))
+
+  (if (string=? x "none"
+(define (parse-namespace-db item)
+  (match item
+    (('dbd dbd) (conf-set! '(db dbd) dbd))
+    (('port port) (conf-set! '(db port) (string->number port)))
+    (('addr addr) (conf-set! '(db addr) addr))
+    (('name name) (conf-set! '(db name) name))
+    (('socket sock) (conf-set '(db sock) sock))
+    (('username username) (conf-set! '(db username) username))
+    (('passwd passwd) (conf-set! '(db passwd) passwd))
+    (else (error parse-namespace-db "Config: Invalid item" item))))
+
+(define (parse-namespace-server item)
+  (match item
+    (('info info) (conf-set! '(server info) (->none/str info)))
+    (('nginx nginx) (conf-set! '(server nginx) (->bool nginx)))
+    (('charset charset) (conf-set! '(server charset) charset))
+    (else (error parse-namespace-server "Config: Invalid item" item))))
+
+(define (parse-namespace-host item)
+  (match item
+    (('addr addr) (conf-set! '(host addr) addr))
+    (('port port) (conf-set! '(host port) (string->number port)))
+    (else (error parse-namespace-host "Config: Invalid item" item))))
+
+(define (parse-namespace-error item)
+  (match item
+    (('path path) (conf-set! '(error path) path))
+    (else (error parse-namespace-error "Config: Invalid item" item))))
+
+(define (parse-namespace-session item)
+  (match item
+    (('path path) (conf-set! '(session path) path))
+    (else (error parse-namespace-session "Config: Invalid item" item))))
+
+(define (parse-namespace-upload item)
+  (match item
+    (('type types) (conf-set! '(upload types) (->list types)))
+    (('path path) (conf-set! '(upload path) path))
+    (else (error parse-namespace-upload "Config: Invalid item" item))))
+
+(define (parse-namespace-mail item)
+  (match item
+    (('sender sender) (conf-set! '(mail sender) sender))
+    (else (error parse-namespace-mail "Config: Invalid item" item))))
 
 (define (parse-config-item item)
   (match item
-    (('db rest ...) (parse-namespace-db item))
-    (('server rest ...) (parse-namespace-server item))
-    (('host rest ...) (parse-namespace-host item))
-    (('error rest ...) (parse-namespace-error item))
-    (('session rest ...) (parse-namespace-session item))
-    (('upload rest ...) (parse-namespace-upload item))
-    (('mail rest ...) (parse-namespace-mail item))
+    (('db rest ...) (parse-namespace-db rest))
+    (('server rest ...) (parse-namespace-server rest))
+    (('host rest ...) (parse-namespace-host rest))
+    (('error rest ...) (parse-namespace-error rest))
+    (('session rest ...) (parse-namespace-session rest))
+    (('upload rest ...) (parse-namespace-upload rest))
+    (('mail rest ...) (parse-namespace-mail rest))
+    (() #t) ; skip
     (else (error parse-config-item "Unsupported config namespace!" item))))
 
-;; TODO: implement all parser
+(define (parse-line line)
+  (call-with-input-string 
+   line
+   (lambda (port)
+     (let lp((next (read-char port)) (key? #t) (word '()) (ret '()))
+       (cond
+        ((or (eof-object? next)
+             (char=? next #\#)) ; skip comment 
+         (reverse (cons (list->string (reverse word)) ret)))
+        ((char-set-contains? char-set:whitespace next)
+         ;; skip all whitespaces
+         (lp (read-char port) key? word ret))
+        ((and key? (char=? next #\.))
+         ;; a namespace end
+         (lp (read-char port) key? '() (cons (list->symbol (reverse word)) ret)))
+        ((and key? (char=? next #\=))
+         ;; value start
+         (lp (read-char port) #f '() (cons (list->symbol (reverse word)) ret)))
+        ((not key?)
+         ;; store chars of value
+         (lp (read-char port) key? (cons next word) ret))
+        (else
+         ;; store chars of key
+         (lp (read-char port) key? (cons next word) ret)))))))
 
-(define (init-config)
-  #t)
+(define (init-inner-database-item)
+  (define dbd (get-conf '(db dbd)))
+  (define port (get-conf '(db port)))
+  (define addr (get-conf '(db addr)))
+  (define sock (get-conf '(db sock)))
+  (define name (get-conf '(db name)))
+  (define username (get-conf '(db username)))
+  (define passwd (get-conf '(db passwd)))
+
+  (cond
+   ((or (and port addr sock)
+        (and port sock)
+        (and addr sock))
+    (error init-inner-database-item "Conf: either addr:port or sock mode!"))
+   ((eq? dbd 'sqlite3) ; sqlite3 needs only dbname
+    (conf-set! 'databse `(sqlite3 ,username ,passwd ,name)))
+   ((and addr port) ; addr:port mode
+    (conf-set! 'database `(,dbd ,username ,passwd ,name (socketfile ,sock))))
+   ((and (eq? dbd 'mysql) sock) ; mysql has socket file mode
+    (conf-set! 'database `(mysql ,username ,passwd ,name (port ,addr ,port))))
+   (else (error init-inner-database-item "Fatal: Invalid database config"))))
+
+(define (init-inner-config-items)
+  (init-inner-database-item))
+
+(define (init-config conf-file)
+  (define fp (open-input-file 
+              (cond
+               ((file-exists? conf-file) conf-file)
+               ((file-exists? *default-conf-file*) *default-conf-file*)
+               (else 
+                (error init-config 
+                       "Fatal error! You need to reinstall Artanis!")))))
+  (let lp((line (read-line fp)))
+    (cond
+     ((eof-object? line) #t)
+     ((parse-line line) 
+      => (lambda (item)
+           (parse-config-item item)
+           (lp (read-line fp))))
+     (else (error init-config "Invalid line" line))))
+  (init-inner-config-items))
