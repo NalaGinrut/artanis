119c8da76c02b71dcab1f5e2e4bdaef4ee0995d9
Nala Ginrut <nalaginrut@gmail.com>
2014-04-26 23:55:24 +0800

    update oht

--- artanis/oht.scm
+++ artanis/oht.scm
@@ -24,6 +24,7 @@
   #:use-module (artanis sql-mapping)
   #:use-module (artanis db)
   #:use-module (artanis cookie)
+  #:use-module (artanis cache)
   #:use-module (srfi srfi-1)
   #:export (new-oht))
 
@@ -37,7 +38,7 @@
     (and h (h rc args ...))))
 
 ;; delay to open conn iff it's required.
-(define-syntax-rule (try-open-a-connection-for-rc rc)
+(define-syntax-rule (try-open-DB-connection-for-rc rc)
   (let ((conn ((@ (artanis artanis) re-conn) rc)))
     (if conn
         conn
@@ -54,13 +55,14 @@
 (define (conn-maker yes? rule keys)
   (and yes?
        (lambda (rc sql)
-         (let ((conn (try-open-a-connection-for-rc rc)))
+         (let ((conn (try-open-DB-connection-for-rc rc)))
            (DB-query conn sql)
            conn))))
   
 ;; NOTE: these handlers should never be exported!
 ;; ((handler arg rule keys) rc . args)
-;; NOTE: If the keyword wasn't specified, the handler should return #f
+;; NOTE: If the keyword wasn't specified while defining the url-remap,
+;;       the handler should return #f.
 (define *opions-meta-handler-table*
   `(;; a short way for sql-mapping from the bindings in rule
     ;; e.g #:sql-mapping "select * from table where id=${:id}"
@@ -71,9 +73,19 @@
     (#:str . str-maker)
 
     ;; short-cut for authentication
-    ;; e.g (get "/login" #:auth "select passwd from users where usr=${usr}"
+    ;; #:auto accepts these values:
+    ;; 1. (table-name username-literal-string passwd-literal-string)
+    ;; 2. (table-name)
+    ;; 3. SQL as string
+    ;; e.g (get "/login" 
+    ;;      #:auth "select 'mypasswd' from blog where usrname='myname'"
     ;;      (lambda (rc)
-    ;;       (:auth #:usr (params rc "usrname") #:passwd (params rc "passwd"))))
+    ;;       (:auth rc #:usrname "myname" #:passwd "mypasswd")
+    ;;       ...))
+    ;; or (get "/login" #:auth '(blog myname mypasswd)
+    ;;      (lambda (rc)
+    ;;       (:auth rc) ...))
+    ;; The #:usrname and #:passwd will be expaned automatically.
     ;; TODO: working on this
     (#:auth . auth-maker)
 
@@ -86,6 +98,27 @@
     ;;      (lambda (rc)
     ;;       (:conn rc "select * from articles")))
     (#:conn . conn-maker)
+
+    ;; Web caching
+    ;; The default value is #f.
+    ;; This is useful for web caching.
+    ;; values:
+    ;; * #t: will store the md5 of this page content, 
+    ;;       and compare it with Etag.
+    ;; * #f: won't store md5, and won't do any caching work.
+    ;;       Etag would be ignored. If the current rule is for dynamic page,
+    ;;       you have to set it to #f.
+    ;; * filename: You may use (cache-file rc) to return the static file,
+    ;;             it's the same function like emit-response-with-file.
+    ;;             The difference is to cache it in memory.
+    ;; * Caveats:
+    ;; 1. #t will generate the dynamic page and get md5 for it anyway, the
+    ;;    only advantage would be saving the bandwidth (return 304).
+    ;; 2. Whether to use #:cache is depending on you.
+    ;; NOTE: this option will be disabled automatically for some reasons:
+    ;; 1. when #:auth was used in the same rule.
+    ;; 2. when there's any HTTP header to stop the caching.
+    (#:cache . cache-maker)
 
     ;; short-cut to set cookies
     ;; e.g (get "/" #:cookies (ca cb)
@@ -102,16 +135,19 @@
 (meta-handler-register str)
 (meta-handler-register conn)
 (meta-handler-register cookies)
+(meta-handler-register cache)
 (define-syntax-rule (:cookies-set! ck k v)
   ((:cookies 'set) ck k v))
 (define-syntax-rule (:cookies-ref ck k)
   ((:cookies 'ref) ck k))
+(define-syntax-rule (:cookies-update! rc)
+  ((:cookies 'update) rc))
 
 (for-each (lambda (x) (meta-handler-register (car x)))
           *opions-meta-handler-table*)
 
 (define-syntax-rule (oh-ref o)
-  (assoq *opions-meta-handler-table* o))
+  (assoc-ref *opions-meta-handler-table* o))
 
 ;; return #f when there's no opt specified
 (define* (new-oht opts #:key (rule 'no-rules) (keys 'no-keys))
