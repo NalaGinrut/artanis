3bffe9fa6d123a2c3873ab841746f47d01a9002f
Nala Ginrut <nalaginrut@gmail.com>
2014-08-25 00:09:21 +0800

    auth-maker works\!

--- artanis/sql-mapping.scm
+++ artanis/sql-mapping.scm
@@ -19,20 +19,23 @@
   #:use-module (artanis db)
   #:use-module (artanis ssql)
   #:use-module (artanis route)
+  #:use-module ((artanis page) #:select (params))
+  #:use-module (artanis crypto base64)
   #:use-module (ice-9 match)
+  #:use-module (srfi srfi-26)
   #:export (sql-mapping-maker
             auth-maker))
 
 ;; TODO:
 ;; 1. sql-mapping should be the only DB abstraction of Artanis(if any possible).
 ;; 2. There should be Anti SQL-Injection Mechanism (ASIM), a static analysis tool would
-;;    be better(challengeable).
+;;    be better(challenging).
 ;; 3. A DSL within ${..} string template, which provides a convinient way to let
 ;;    user specify ASIM options for the field passed from the client.
 ;; 4. DSL should handle key-value or list for users easily.
 ;; 5. DSL should handle Stored-Procedure for specific DB implementation (as possible).
 ;; 6. There's no ORM, but FPRM. The only difference is that you never see
-;;    Classes but Closures. Hmm...what's the essential difference?
+;;    Classes but Closures. Hmm...what's the essential difference? Stateless, of course!
 
 (define (sql-mapping-maker sql-tpl rule keys)
   #f)
@@ -52,32 +55,68 @@
   (define mode #f)
   (define passwd "passwd")
   (define username "username")
-  (define-syntax-rule (checker r pw str)
-    (string=? (crypto (params r pw))
-              (DB-query (DB-open rc) str)))
+  (define-syntax-rule (->passwd rc sql)
+    (assoc-ref (DB-get-top-row (DB-query (DB-open rc) sql)) passwd))
+  (define (table-checker rc sql)
+    (string=? (crypto (params rc passwd)) (->passwd rc sql)))
+  (define customed-basic-checker #f)
+  (define (basic-checker rc p sql)
+    (format #t "~a, ~a~%" p sql)
+    (string=? p (->passwd rc sql)))
   (define sql
     (match val
-      (((? symbol? table) username-field passwd-field crypto-proc)
-       (set! crypto crypto-proc)
-       (set! mode 'specified-field)
+      (`(table ,table ,username-field ,passwd-field)
+       (set! mode 'table-specified-fields)
        (set! passwd passwd-field)
        (set! username username-field)
-       (->sql select passwd-field from table (where #:usrname username-field)))
-      (((? symbol? table) crypto-proc)
+       (lambda (u)
+         (->sql select (passwd-field) from table (where (string->keyword username-field) u))))
+      (`(table ,table ,username-field ,passwd-field ,crypto-proc)
+       (set! mode 'table-specified-fields)
        (set! crypto crypto-proc)
-       (set! mode 'only-table)
-       (->sql select passwd from table (where #:usrname username)))
+       (set! passwd passwd-field)
+       (set! username username-field)
+       (lambda (u)
+         (->sql select (passwd-field) from table (where (string->keyword username-field) u))))
+      (`(table ,table ,crypto-proc)
+       (set! mode 'table)
+       (set! crypto crypto-proc)
+       (lambda (u)
+         (->sql select (passwd) from table (where (string->keyword username) u))))
+      (`(basic ,table ,username-field ,passwd-field)
+       (set! passwd-field passwd)
+       (set! mode 'basic)
+       (lambda (u)
+         (->sql select (passwd) from table (where (string->keyword username-field) u))))
+      (`(basic ,checker)
+       (set! mode 'basic)
+       (set! customed-basic-checker checker)
+       #f)
       ((? string? tpl)
        (set! mode 'tpl)
        (make-db-string-template tpl))
       (else (error auth-maker "wrong pattern"))))
   (lambda (rc . kargs)
-    (match mode
-      ('specified-field
-       (checker rc passwd sql))
-      ('only-table
-       (checker rc passwd sql))
-      ('tpl
-       (apply sql `(,@(alist->kblist (rc-bt rc)) ,@kargs)))
-      (else 
-       (throw 'artanis-err 500 "auth-maker: Invalid mode!" mode)))))
+    (define result
+      (case mode
+        ((table) (table-checker rc sql))
+        ((table-specified-fields)
+         (let ((u (params rc username))
+               (p (params rc passwd)))
+           (format #t "~a: ~a~%" username u)
+           (format #t "~a: ~a~%" passwd p)
+           (and u (table-checker rc (sql u)))))
+        ((tpl) (table-checker rc (apply sql kargs)))
+        ((basic)
+         (match (get-header rc 'authorization)
+           (`(basic . ,(= base64-decode (= (cut string-split <> #\:) up)))
+            (let ((u (car up)) (p (cadr up)))
+              (if customed-basic-checker
+                  (customed-basic-checker rc u p)
+                  (basic-checker rc p (sql u)))))
+           (else #f)))
+        (else (throw 'artanis-err 500 "auth-maker: Fatal BUG! Invalid mode! Shouldn't be here!" mode))))
+    (if result
+        (display "Auth ok!\n")
+        (display "Auth failed!\n"))
+    result))
