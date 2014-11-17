36708deb353b4a8d840301bdae6c06e1181b7e97
Nala Ginrut <nalaginrut@gmail.com>
2014-11-17 18:53:17 +0800

    added (artanis sql-mapping mapping)

---
+++ artanis/sql-mapping/mapping.scm
@@ -0,0 +1,113 @@
+;;  -*-  indent-tabs-mode:nil; coding: utf-8 -*-
+;;  Copyright (C) 2014
+;;      "Mu Lei" known as "NalaGinrut" <NalaGinrut@gmail.com>
+;;  Artanis is free software: you can redistribute it and/or modify
+;;  it under the terms of the GNU General Public License as published by
+;;  the Free Software Foundation, either version 3 of the License, or
+;;  (at your option) any later version.
+
+;;  Artanis is distributed in the hope that it will be useful,
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;  GNU General Public License for more details.
+
+;;  You should have received a copy of the GNU General Public License
+;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (artanis sql-mapping mapping)
+  #:use-module (artanis irregex)
+  #:use-module (ice-9 receive)
+  #:use-module (srfi srfi-1)
+  #:use-module ((rnrs) #:select (get-string-all))
+  #:export (make-sm-string-template))
+
+(define (sm-name-parser name)
+  ;; NOTE: it's ok if there's strange symbols in the name, since it'll never
+  ;;       be referenced by the template.
+  (define (get-rest port)
+    (string->symbol (string-trim-both (get-string-all port))))
+  (define (%parser port)
+    (let ((c (peek-char port)))
+      (cond
+       ((eof-object? c)
+        (throw 'artanis-err 500 "sm-name-parser: Shouldn't be empty name!" name))
+       ((char=? c #\@) ; post-qstr
+        (read-char port) ; skip #\@
+        `(post-qstr ,(get-rest port)))
+       (else `(name ,(get-rest port))))))
+  (call-with-input-string name %parser))
+
+;; A modification of %make-string-template to support special syntax
+;; of sql-mapping.
+(define (make-sm-string-template template)
+  (define irx (sre->irregex '(or (=> dollar "$$")
+                                 (: "${" (=> var (+ (~ #\}))) "}"))))
+  (define *post-table* '())
+  (define (post-table-add! k v)
+    (set! *post-table* (assq-set! *post-table* k v)))
+  (define (post-table-ref k) (assq-ref *post-table* k))
+  (define (->string rc obj)
+    (if (string? obj) obj (object->string obj)))
+  (define (optimize rev-items tail)
+    (cond ((null? rev-items) tail)
+          ((not (string? (car rev-items)))
+           (optimize (cdr rev-items)
+                     (cons (car rev-items) tail)))
+          (else (receive (strings rest) (span string? rev-items)
+                  (let ((s (string-concatenate-reverse strings)))
+                    (if (string-null? s)
+                        (optimize rest tail)
+                        (optimize rest (cons s tail))))))))
+  (define (parse-it name)
+    (let ((nn (sm-name-parser name)))
+      (match nn
+        (`(name ,n) (names-table-add! n 'name))
+        (`(post-qstr ,pn) (names-table-add! pn 'post-qstr))
+        (else (throw 'artanis-err 500 "make-sm-string-template: Invalid result after parse"
+                     nn)))))
+  (define (match->item m)
+    (or (and (irregex-match-substring m 'dollar) "$")
+        (let ((name (irregex-match-substring m 'var)))
+          (parse-it name)
+          (list name))))
+  (let* ((rev-items (irregex-fold
+                     irx
+                     (lambda (idx m tail)
+                       (cons* (match->item m)
+                              (substring template
+                                         idx
+                                         (irregex-match-start-index m 0))
+                              tail))
+                     '()
+                     template
+                     (lambda (idx tail)
+                       (cons (substring template idx) tail))))
+         (items (optimize rev-items '())))
+    (lambda (rc . kargs)
+      (define (item->string item)
+        (if (string? item)
+            item
+            (cond
+             ((get-the-val kargs (car item)) => ->string)
+             ((qstr-ref rc (car item)) => ->string)
+             ((cdr item) (cdr item))
+             (else (error "Missing keyword" (car item))))))
+      (define post-data #f)
+      (define (get-post-data rc)
+        (define (-> x)
+          (string-trim-both x (lambda (c) (member c '(#\sp #\: #\return)))))
+        (if post-data
+            post-data
+            (if (rc-body rc)
+                (set! post-data
+                      (map (lambda (x)
+                             (map -> (string-split x #\=)))
+                           (string-split ((@ (rnrs) utf8->string) (rc-body rc)) #\&)))
+                '())))
+      (define (post-ref key) (and=> (assoc-ref (get-post-data rc) key) car))
+      (define (get-the-val lst key)
+        (let ((str (kw-arg-ref lst key)))
+          (string-concatenate (list "\"" (->string rc str) "\""))))
+      (define (qstr-ref k)
+        (and (post-table-ref k) (post-ref k)))
+      (string-concatenate (append (map qstr-ref items) (map item->string items))))))
