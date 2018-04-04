ee7af691f7c6c0acd4eef9d8a5dcc15ed3f1a4ff
Nala Ginrut <nalaginrut@gmail.com>
2018-04-05 02:20:53 +0800

    replaced old base64 implementation with industria version

--- artanis/crypto/base64.scm
+++ artanis/crypto/base64.scm
@@ -1,142 +1,227 @@
 ;;  -*-  indent-tabs-mode:nil; coding: utf-8 -*-
-;; Copyright (C) 2008, 2010, 2012 Andy Wingo <wingo at pobox dot com>
-;; Copyright (C) 2009 Andreas Rottmann <a dot rottmann at gmx dot at>
-;; Copyright (C) 2013,2014,2015
-;; Mu Lei known as NalaGinrut <mulei@gnu.org>
 
-;;  Artanis is free software: you can redistribute it and/or modify
-;;  it under the terms of the GNU General Public License and GNU
-;;  Lesser General Public License published by the Free Software
-;;  Foundation, either version 3 of the License, or (at your option)
-;;  any later version.
+;; Copyright © 2009, 2010, 2012, 2013 Göran Weinholt <goran@weinholt.se>
+;; Copyright 2018 Mu Lei known as NalaGinrut <nalaginrut@gmail.com>
 
-;;  Artanis is distributed in the hope that it will be useful,
-;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;  GNU General Public License and GNU Lesser General Public License
-;;  for more details.
+;; Permission is hereby granted, free of charge, to any person obtaining a
+;; copy of this software and associated documentation files (the "Software"),
+;; to deal in the Software without restriction, including without limitation
+;; the rights to use, copy, modify, merge, publish, distribute, sublicense,
+;; and/or sell copies of the Software, and to permit persons to whom the
+;; Software is furnished to do so, subject to the following conditions:
 
-;;  You should have received a copy of the GNU General Public License
-;;  and GNU Lesser General Public License along with this program.
-;;  If not, see <http://www.gnu.org/licenses/>.
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
 
-;;; Commentary:
-;;
-;; base 64 y'all
-;;
-;;; Code:
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+;; THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+;; FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+;; DEALINGS IN THE SOFTWARE.
 
-(define-module (artanis crypto base64)
-  #:use-module (rnrs bytevectors)
-  #:export (base64-encode base64-decode))
+;; RFC 4648 Base-N Encodings
 
-(define b64-bytes
-  (string->utf8
-   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwzyz0123456789+/"))
+#!r6rs
 
-(define (int->b64-byte i)
-  (bytevector-u8-ref b64-bytes (logand i 63)))
+(library (artanis crypto base64)
+  (export base64-encode base64-decode)
+  (import (rnrs))
 
-(define b64-byte-ranges
-  (map cons
-       (map char->integer '(#\A #\a #\0 #\+ #\/))
-       (map char->integer '(#\Z #\z #\9 #\+ #\/))))
+  (define base64-alphabet
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/")
 
-(define (b64-byte->int i)
-  (let lp ((ranges b64-byte-ranges) (out 0))
-    (cond ((null? ranges)
-           (error "bad base64 byte" i))
-          ((and (>= i (caar ranges)) (<= i (cdar ranges)))
-           (+ out (- i (caar ranges))))
-          (else
-           (lp (cdr ranges) (+ out (+ 1 (- (cdar ranges)
-                                           (caar ranges)))))))))
+  (define base64url-alphabet
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_")
 
-(define (bytevector-pad bv n fill)
-  (let ((result (make-bytevector n fill)))
-    (bytevector-copy! bv 0 result 0 (bytevector-length bv))
-    result))
+  (define %base64-encode
+    (case-lambda
+      ;; Simple interface. Returns a string containing the canonical
+      ;; base64 representation of the given bytevector.
+      ((bv)
+       (%base64-encode bv 0 (bytevector-length bv) #f #f base64-alphabet #f))
+      ((bv start)
+       (%base64-encode bv start (bytevector-length bv) #f #f base64-alphabet #f))
+      ((bv start end)
+       (%base64-encode bv start end #f #f base64-alphabet #f))
+      ((bv start end line-length)
+       (%base64-encode bv start end line-length #f base64-alphabet #f))
+      ((bv start end line-length no-padding)
+       (%base64-encode bv start end line-length no-padding base64-alphabet #f))
+      ((bv start end line-length no-padding alphabet)
+       (%base64-encode bv start end line-length no-padding alphabet #f))
+      ;; Base64 encodes the bytes [start,end[ in the given bytevector.
+      ;; Lines are limited to line-length characters (unless #f),
+      ;; which must be a multiple of four. To omit the padding
+      ;; characters (#\=) set no-padding to a true value. If port is
+      ;; #f, returns a string.
+      ((bv start end line-length no-padding alphabet port)
+       (assert (or (not line-length) (zero? (mod line-length 4))))
+       (let-values (((p extract) (if port
+                                     (values port (lambda () (values)))
+                                     (open-string-output-port))))
+         (letrec ((put (if line-length
+                           (let ((chars 0))
+                             (lambda (p c)
+                               (when (fx=? chars line-length)
+                                 (set! chars 0)
+                                 (put-char p #\linefeed))
+                               (set! chars (fx+ chars 1))
+                               (put-char p c)))
+                           put-char)))
+           (let lp ((i start))
+             (cond ((= i end))
+                   ((<= (+ i 3) end)
+                    (let ((x (bytevector-uint-ref bv i (endianness big) 3)))
+                      (put p (string-ref alphabet (fxbit-field x 18 24)))
+                      (put p (string-ref alphabet (fxbit-field x 12 18)))
+                      (put p (string-ref alphabet (fxbit-field x 6 12)))
+                      (put p (string-ref alphabet (fxbit-field x 0 6)))
+                      (lp (+ i 3))))
+                   ((<= (+ i 2) end)
+                    (let ((x (fxarithmetic-shift-left (bytevector-u16-ref bv i (endianness big)) 8)))
+                      (put p (string-ref alphabet (fxbit-field x 18 24)))
+                      (put p (string-ref alphabet (fxbit-field x 12 18)))
+                      (put p (string-ref alphabet (fxbit-field x 6 12)))
+                      (unless no-padding
+                        (put p #\=))))
+                   (else
+                    (let ((x (fxarithmetic-shift-left (bytevector-u8-ref bv i) 16)))
+                      (put p (string-ref alphabet (fxbit-field x 18 24)))
+                      (put p (string-ref alphabet (fxbit-field x 12 18)))
+                      (unless no-padding
+                        (put p #\=)
+                        (put p #\=)))))))
+         (extract)))))
 
-(define-syntax bytevector-map-n-to-m
-  (lambda (stx)
-    (syntax-case stx ()
-      ((_ n m)
-       (with-syntax (((byte-in ...)
-                      (map (lambda (x)
-                             #`(bytevector-u8-ref s (+ i #,x)))
-                           (iota (syntax->datum #'n))))
-                     ((byte-out ...)
-                      (generate-temporaries (iota (syntax->datum #'m))))
-                     ((byte-out-idx ...)
-                      (iota (syntax->datum #'m))))
-         #'(lambda (proc s)
-             (let* ((len (bytevector-length s))
-                    (out (make-bytevector (* len (/ m n)))))
-               (let lp ((i 0) (j 0))
-                 (cond
-                  ((< i len)
-                   (call-with-values (lambda () (proc byte-in ...))
-                     (lambda (byte-out ...)
-                       (bytevector-u8-set! out (+ j byte-out-idx) byte-out)
-                       ...))
-                   (lp (+ i n) (+ j m)))
-                  (else out))))))))))
+  ;; Create a lookup table for the alphabet and remember the latest table.
+  (define get-decode-table
+    (let ((ascii-table #f)
+          (extra-table '())     ;in the unlikely case of unicode chars
+          (table-alphabet #f))
+      (lambda (alphabet)
+        (unless (eq? alphabet table-alphabet)
+          ;; Rebuild the table.
+          (do ((ascii (make-vector 128 #f))
+               (extra '())
+               (i 0 (+ i 1)))
+              ((= i (string-length alphabet))
+               (set! ascii-table ascii)
+               (set! extra-table extra))
+            (let ((c (char->integer (string-ref alphabet i))))
+              (if (fx<=? c 127)
+                  (vector-set! ascii c i)
+                  (set! extra (cons (cons c i) extra)))))
+          (set! table-alphabet alphabet))
+        (values ascii-table extra-table))))
 
-(define (bytevector-fill-range! bv start end u8)
-  (do ((i (- end 1) (- i 1)))
-      ((< i start))
-      (bytevector-u8-set! bv i u8)))
+  ;; Decodes a correctly padded base64 string, optionally ignoring
+  ;; non-alphabet characters.
+  (define %base64-decode
+    (case-lambda
+      ((str)
+       (%base64-decode str base64-alphabet #f))
+      ((str alphabet)
+       (%base64-decode str alphabet #f))
+      ((str alphabet port)
+       (%base64-decode str alphabet port #t))
+      ((str alphabet port strict?)
+       (define (pad? c) (eqv? c (char->integer #\=)))
+       (let-values (((p extract) (if port
+                                     (values port (lambda () (values)))
+                                     (open-bytevector-output-port)))
+                    ((ascii extra) (get-decode-table alphabet)))
+         (define-syntax lookup
+           (syntax-rules ()
+             ((_ c) (or (and (fx<=? c 127) (vector-ref ascii c))
+                        (cond ((assv c extra) => cdr)
+                              (else #f))))))
+         (let* ((len (if strict?
+                         (string-length str)
+                         (let lp ((i (fx- (string-length str) 1)))
+                           ;; Skip trailing invalid chars.
+                           (cond ((fxzero? i) 0)
+                                 ((let ((c (char->integer (string-ref str i))))
+                                    (or (lookup c) (pad? c)))
+                                  (fx+ i 1))
+                                 (else (lp (fx- i 1))))))))
+           (let lp ((i 0))
+             (cond
+              ((fx=? i len)
+               (extract))
+              ((fx<=? i (fx- len 4))
+               (let lp* ((c1 (char->integer (string-ref str i)))
+                         (c2 (char->integer (string-ref str (fx+ i 1))))
+                         (c3 (char->integer (string-ref str (fx+ i 2))))
+                         (c4 (char->integer (string-ref str (fx+ i 3))))
+                         (i i))
+                 (let ((i1 (lookup c1)) (i2 (lookup c2))
+                       (i3 (lookup c3)) (i4 (lookup c4)))
+                   (cond
+                    ((and i1 i2 i3 i4)
+                     ;; All characters present and accounted for.
+                     ;; The most common case.
+                     (let ((x (fxior (fxarithmetic-shift-left i1 18)
+                                     (fxarithmetic-shift-left i2 12)
+                                     (fxarithmetic-shift-left i3 6)
+                                     i4)))
+                       (put-u8 p (fxbit-field x 16 24))
+                       (put-u8 p (fxbit-field x 8 16))
+                       (put-u8 p (fxbit-field x 0 8))
+                       (lp (fx+ i 4))))
+                    ((and i1 i2 i3 (pad? c4) (= i (- len 4)))
+                     ;; One padding character at the end of the input.
+                     (let ((x (fxior (fxarithmetic-shift-left i1 18)
+                                     (fxarithmetic-shift-left i2 12)
+                                     (fxarithmetic-shift-left i3 6))))
+                       (put-u8 p (fxbit-field x 16 24))
+                       (put-u8 p (fxbit-field x 8 16))
+                       (lp (fx+ i 4))))
+                    ((and i1 i2 (pad? c3) (pad? c4) (= i (- len 4)))
+                     ;; Two padding characters.
+                     (let ((x (fxior (fxarithmetic-shift-left i1 18)
+                                     (fxarithmetic-shift-left i2 12))))
+                       (put-u8 p (fxbit-field x 16 24))
+                       (lp (fx+ i 4))))
+                    ((not strict?)
+                     ;; Non-alphabet characters.
+                     (let lp ((i i) (c* '()) (n 4))
+                       (cond ((fxzero? n)
+                              ;; Found four valid characters.
+                              (lp* (cadddr c*) (caddr c*) (cadr c*) (car c*)
+                                   (fx- i 4)))
+                             ((fx=? i len)
+                              (error '%base64-decode
+                                     "Invalid input in non-strict mode."
+                                     i c*))
+                             (else
+                              ;; Gather alphabetic (or valid
+                              ;; padding) characters.
+                              (let ((c (char->integer (string-ref str i))))
+                                (cond ((or (lookup c)
+                                           (and (pad? c)
+                                                (fx<=? n 2)
+                                                (fx=? i (fx- len n))))
+                                       (lp (fx+ i 1) (cons c c*) (fx- n 1)))
+                                      (else
+                                       (lp (fx+ i 1) c* n))))))))
+                    (else
+                     (error '%base64-decode
+                            "Invalid input in strict mode."
+                            c1 c2 c3 c4))))))
+              (else
+               (error '%base64-decode
+                      "The input is too short, it may be missing padding."
+                      i)))))))))
 
-(define (bytevector-copy/padding bv npad pad-byte)
-  (let ((result (bytevector-copy bv))
-        (len (bytevector-length bv)))
-    (bytevector-fill-range! result (- len npad) len pad-byte)
-    result))
+  (define (get-line-comp f port)
+    (if (port-eof? port)
+        (eof-object)
+        (f (get-line port))))
 
-(define (base64-encode str/bv)
-  (let* ((bv (if (string? str/bv) (string->utf8 str/bv) str/bv))
-         (npad (remainder (- 3 (remainder (bytevector-length bv) 3)) 3))
-         (out ((bytevector-map-n-to-m 3 4)
-               (lambda (x y z)
-                 (let ((n (logior (ash x 16) (ash y 8) z)))
-                   (values (int->b64-byte (ash n -18))
-                           (int->b64-byte (ash n -12))
-                           (int->b64-byte (ash n -6))
-                           (int->b64-byte n))))
-               (bytevector-pad bv (+ (bytevector-length bv) npad) 0))))
-    (bytevector-fill-range! out
-                            (- (bytevector-length out) npad)
-                            (bytevector-length out)
-                            (char->integer #\=))
-    (utf8->string out)))
+  (define (base64-encode str/bv)
+    (%base64-encode (if (string? str/bv) (string->utf8 str/bv) str/bv)))
 
-(define eql-byte (char->integer #\=))
-
-(define (b64-bv-npad bv)
-  (let ((len (bytevector-length bv)))
-    (if (> len 0)
-        (if (= (bytevector-u8-ref bv (- len 1)) eql-byte)
-            (if (> len 1)
-                (if (= (bytevector-u8-ref bv (- len 2)) eql-byte)
-                    2
-                    1)
-                1)
-            0)
-        0)))
-
-(define* (base64-decode str #:optional (outstr #t))
-  (let* ((bv (string->utf8 str))
-         (npad (b64-bv-npad bv))
-         (out ((bytevector-map-n-to-m 4 3)
-               (lambda (w x y z)
-                 (let ((n (logior (ash (b64-byte->int w) 18)
-                                  (ash (b64-byte->int x) 12)
-                                  (ash (b64-byte->int y) 6)
-                                  (b64-byte->int z))))
-                   (values (ash n -16)
-                           (logand (ash n -8) 255)
-                           (logand n 255))))
-               (bytevector-copy/padding bv npad (char->integer #\A))))
-         (result (make-bytevector (- (bytevector-length out) npad))))
-    (bytevector-copy! out 0 result 0 (bytevector-length result))
-    (if outstr (utf8->string result) result)))
+  (define (base64-decode str/bv)
+    (%base64-decode (if (string? str/bv) (string->utf8 str/bv) str/bv))))
