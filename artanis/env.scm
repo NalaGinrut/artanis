656480e7ea0109be1f63d6a4548bbf0fcb61512e
Nala Ginrut <mulei@gnu.org>
2019-07-11 15:20:45 +0800

    Remove redundant debugn info from env.scm

--- artanis/env.scm
+++ artanis/env.scm
@@ -234,7 +234,6 @@
 ;; (client-fd . mmapped-bv)
 (define *mmapped-bv-from-file* (make-hash-table))
 (define (register-mmapped-file! fd bv)
-  (DEBUG "Register mmaped file for ~a~%" fd)
   (hashv-set! *mmapped-bv-from-file* fd bv))
 (define (is-mmapped-file? fd)
   (hashv-ref *mmapped-bv-from-file* fd #f))
