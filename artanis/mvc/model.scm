17063148027242d9bb3082ef40bbcc526c51877e
NalaGinrut <mulei@gnu.org>
2022-06-19 02:10:54 +0800

    Fix MVC/model to support native DB types

--- artanis/mvc/model.scm
+++ artanis/mvc/model.scm
@@ -113,7 +113,9 @@
     ;; Fractional part is the number of digits following the decimal point.
     ((float) `(float ,@(get-integer-fractional-part opts)))
     ((double) `(double ,@(get-integer-fractional-part opts)))
-    ((char-field) `(varchar ,@(get-maxlen opts)))))
+    ((char-field) `(varchar ,@(get-maxlen opts)))
+    ((date-field) (apply date-field-handler name opts))
+    (else `(,name ,opts))))
 
 (define (date-field-handler now . opts)
   (let ((new-opts
@@ -123,23 +125,6 @@
                  ((auto-once) '(#:auto-now-once))
                  (else '())))))
     (list 'date (opts-add new-opts opts))))
-
-(define *model-field-handlers*
-  `((auto . ,general-field-handler)
-    (tiny-integer . ,general-field-handler)
-    (small-integer . ,general-field-handler)
-    (big-integer . ,general-field-handler)
-    (boolean . ,general-field-handler)
-    (float . ,general-field-handler)
-    (double . ,general-field-handler)
-    (text . ,general-field-handler)
-    (char-field . ,general-field-handler)
-    (date-field . ,date-field-handler)))
-
-;; handler returns a list contains the valid field type in certain DBD
-(define (model-field-add! name handler)
-  (set! *model-field-handlers*
-    (assoc-set! *model-field-handlers* name handler)))
 
 ;; NOTE: Different from auto_now* in Django, we allow users pass new value
 ;;       even auto_now* was specified.
@@ -219,7 +204,7 @@
 
 (define (field-validator-add! type validator)
   (set! *field-validators*
-    (assoc-set! *field-validators* type validator)))
+        (assoc-set! *field-validators* type validator)))
 
 (define (general-field-validator v)
   ;; TODO: maybe do some security check?
@@ -273,11 +258,7 @@
 
 (define (parse-raw-fields lst)
   (define (->type type opts)
-    (let ((h (assoc-ref *model-field-handlers* type)))
-      (if h
-          (apply h type opts)
-          (throw 'artanis-err 500 parse-raw-fields
-                 "Invalid field type `~a'!" type))))
+    (apply general-field-handler type opts))
   (let lp((next lst) (main '()) (options '()))
     (match next
       (() `(,(reverse main) ,@(reverse options)))
