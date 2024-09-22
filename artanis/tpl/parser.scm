96ded1c11541abd09261ce4adc4b8fe6d232327d
Nala Ginrut <mulei@gnu.org>
2024-09-22 11:02:47 +0900

    Update copyright date

--- artanis/tpl/parser.scm
+++ artanis/tpl/parser.scm
@@ -1,5 +1,5 @@
 ;;  -*-  indent-tabs-mode:nil; coding: utf-8 -*-
-;;  Copyright (C) 2013,2014,2015,2016,2017,2018,2019,2021,2022
+;;  Copyright (C) 2013-2022,2024
 ;;      "Mu Lei" known as "NalaGinrut" <NalaGinrut@gmail.com>
 ;;  Artanis is free software: you can redistribute it and/or modify
 ;;  it under the terms of the GNU General Public License and GNU
