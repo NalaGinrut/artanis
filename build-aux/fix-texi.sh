27598601fdd6a14a58a86ccf7da9c02aaa30daa4
Nala Ginrut <mulei@gnu.org>
2025-01-01 03:57:18 +0900

    Fix docs

--- build-aux/fix-texi.sh
+++ build-aux/fix-texi.sh
@@ -2,5 +2,5 @@
 
 
 # remove @node Top and @top GNU Artanis web-framework Manual from docs/artanis.texi
-sed -i '/^@node Top/d' docs/artanis.texi
-sed -i '/^@top GNU Artanis web-framework Manual/d' docs/artanis.texi
+sed -i '/^@node Top/d' $1/docs/artanis.texi
+sed -i '/^@top GNU Artanis web-framework Manual/d' $1/docs/artanis.texi
